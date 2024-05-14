37bc882939281ca1444488ff118ab1cbd917dc72
dci-student <glaventzakis@gmail.com>
2024-05-14 12:45:29 +0200

    second commit

--- src/main/java/org/example/RestaurantApplication.java
+++ src/main/java/org/example/RestaurantApplication.java
@@ -1,4 +1,14 @@
-package org.example.model;
+package org.example;
 
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // This annotation is used to mark the class as a Spring Boot application
 public class RestaurantApplication {
+
+      public static void main(String[] args) {
+     SpringApplication.run(RestaurantApplication.class, args); // This line of code is used to start the Spring Boot application
+      }
 }
--- src/main/java/org/example/model/Dish.java
+++ src/main/java/org/example/model/Dish.java
@@ -8,4 +8,6 @@ import javax.persistence.*;
 public class Dish {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 }
